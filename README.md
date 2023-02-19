aa59b079a755927fa9973a7887170711b80c369f
Ivan Bozhanov <git@vakata.com>
2023-02-20 01:38:56 +0200

    3.3.15 master

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jstree
+# درخت واره 
 
 [jsTree](http://www.jstree.com/) is jquery plugin, that provides interactive trees. It is absolutely free, [open source](https://github.com/vakata/jstree) and distributed under the MIT license.
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vakata/jstree',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8c1bcc87771f5be0b70cd49d6a7888707ebcc64e',
+        'type' => 'component',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'components/jquery' => array(
+            'pretty_version' => '3.6.0',
+            'version' => '3.6.0.0',
+            'reference' => '6cf38ee1fd04b6adf8e7dda161283aa35be818c3',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/jquery',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vakata/jstree' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8c1bcc87771f5be0b70cd49d6a7888707ebcc64e',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
